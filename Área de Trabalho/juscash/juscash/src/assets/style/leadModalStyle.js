ddf8db1d3811705fff25fc37b33e6386cb07da7d
Raísa Peixoto <raisacad.engcivil@gmail.com>
2023-11-03 11:50:34 -0300

    feat: lead modal

--- Área de Trabalho/juscash/juscash/src/assets/style/leadModalStyle.js
+++ Área de Trabalho/juscash/juscash/src/assets/style/leadModalStyle.js
@@ -1,29 +1,124 @@
 import styled from "styled-components";
 import { COLORS } from "../../constants/colors";
-
+import { CloseOutline } from "@styled-icons/evaicons-outline/CloseOutline";
 const ModalPageContainer = styled.div`
-position:fixed;
-z-index:5;
-top:0;
-right:0;
-width:100vw;
-height:100vh;
-background-color: rgba(255, 255, 255, 0.5);
-backdrop-filter: blur(5px); 
-display:flex;
-align-items:center;
-justify-content:center;
-`
-
-const ModalContainer=styled.div`
-background-color: ${COLORS.white};
-width:50%;
-height:50%;
-`
-
+  position: fixed;
+  z-index: 5;
+  top: 0;
+  right: 0;
+  width: 100vw;
+  height: 100vh;
+  background-color: rgba(255, 255, 255, 0.5);
+  backdrop-filter: blur(5px);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+const CloseIcon = styled(CloseOutline)`
+  width: 25px;
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  color: ${COLORS.darkGray};
+  cursor: pointer;
+`;
+const ModalContainer = styled.div`
+  position: relative;
+  background-color: ${COLORS.white};
+  padding: 30px 10px 20px 30px;
+  max-width: 600px;
+  width: 90%;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  h4 {
+    margin-bottom: 30px;
+    color: ${COLORS.darkGray};
+    font-size: 25px;
+    font-weight: 600;
+  }
+`;
+const OpportunitiesContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
 const Form = styled.form`
-input{
+  h5 {
+    margin-left: 5px;
+    color: ${COLORS.darkGray};
+    font-size: 22px;
+    font-weight: 600;
+    margin-bottom: 10px;
+  }
+  input[type="text"],
+  input[type="email"],
+  input[type="tel"] {
+    margin: 10px 0;
+    width: 90%;
+    height: 40px;
+    padding: 0 10px;
+  }
 
-}
-`
-export{ModalPageContainer,ModalContainer,Form}
+ label{
+    display:flex;
+    align-items:center;
+    color:${COLORS.darkGray};
+    margin-top:10px;
+ }
+  input[type="checkbox"] {
+    -webkit-appearance: none;
+    background-color: white;
+    border: 1px solid ${COLORS.gray};
+    width: 18px;
+    height: 18px;
+    margin-right: 5px;
+    position: relative;
+  }
+
+  input[type="checkbox"]:checked {
+    border:none;
+    background-color: ${COLORS.lightBlue};
+  }
+
+  input[type="checkbox"]:checked::before {
+    content: '✔'; 
+    display: block;
+    color: white; 
+    font-size: 15px; 
+    text-align: center;
+    line-height: 15px;
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+const ButtonsContainer = styled.div`
+  margin-top: 20px;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  button {
+    cursor: pointer;
+    padding: 5px 20px;
+    border: 0.5px solid ${COLORS.gray};
+    background-color: ${COLORS.white};
+    color: ${COLORS.gray};
+    border-radius: 5px;
+  }
+  button[type="submit"] {
+    margin-left: 20px;
+    border: none;
+    color: ${COLORS.white};
+    background-color: ${COLORS.lightBlue};
+  }
+`;
+export {
+  ModalPageContainer,
+  ModalContainer,
+  Form,
+  CloseIcon,
+  OpportunitiesContainer,
+  ButtonsContainer,
+};
--- Área de Trabalho/juscash/juscash/src/controllers/HomeController.js
+++ Área de Trabalho/juscash/juscash/src/controllers/HomeController.js
@@ -4,17 +4,27 @@ import { useNavigate } from "react-router-dom";
 import { Home } from "../pages/index.js";
 import { Loading } from "../components/index.js";
 import { STEPS } from "../constants/steps.js";
-import { getUserLeads,updateLeadStep  } from "../services/LeadsService.js";
+import {
+  getUserLeads,
+  updateLeadStep,
+  postNewLead,
+} from "../services/LeadsService.js";
 import { toast } from "react-toastify";
-
-
+import { OPPORTUNITIES } from "../constants/opportunities.js";
 function HomeController() {
   const { user } = useContext(AuthContext);
   const navigate = useNavigate();
   const [loading, setLoading] = useState(true);
-  const [showLeadModal,setShowLeadModal]=useState(false);
-  const leads = getUserLeads(user?.id) ;
-  /*     [{id:0,user_id:"lofp4tot16lp9",step_id:0,name:"AWS Advocacia",email:"sadsadsa",telefone:"312312",opportunities_id:[0]}] */
+  const [showNewLeadModal, setShowNewLeadModal] = useState(false);
+  const [showLeadModal, setShowLeadModal] = useState(false);
+  const [leadForm, setLeadForm] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    opportunities: [],
+  });
+  const leads = getUserLeads(user?.id); //COLOCAR USE EFFECT
+
   const [leadsList, setLeadsList] = useState(leads);
   useEffect(() => {
     if (!user) {
@@ -22,17 +32,57 @@ function HomeController() {
     }
     setTimeout(() => {
       setLoading(false);
-    }, 2000); 
-    
+    }, 1500);
   }, []);
-  
-  function handleLeadModal(){
-    console.log(showLeadModal)
-    setShowLeadModal(!showLeadModal)
+  function handleLeadForm(e) {
+    const { name, value, type, checked } = e.target;
+
+    if (type === "checkbox") {
+      if (value === "all") {
+        if (checked) {
+          checkAllOpportunitiesLead();
+        } else {
+          setLeadForm({
+            ...leadForm,
+            opportunities: [],
+          });
+        }
+
+        return;
+      }
+      if (checked) {
+        setLeadForm({
+          ...leadForm,
+          opportunities: [...leadForm.opportunities, value],
+        });
+      } else {
+        setLeadForm({
+          ...leadForm,
+          opportunities: leadForm.opportunities.filter(
+            (opportunity) => opportunity !== value
+          ),
+        });
+      }
+    } else {
+      setLeadForm({ ...leadForm, [name]: value });
+    }
+
+    console.log(leadForm);
   }
-
-
-
+  function openNewLeadModal() {
+    console.log(showNewLeadModal);
+    setShowNewLeadModal(true);
+    checkAllOpportunitiesLead();
+  }
+  function checkAllOpportunitiesLead() {
+    const allOpportunities = OPPORTUNITIES.map((o) => {
+      return o.id;
+    });
+    setLeadForm({
+      ...leadForm,
+      opportunities: allOpportunities,
+    });
+  }
   const handleDragStart = (e, leadId, originStepId) => {
     e.dataTransfer.setData("leadId", leadId);
     e.dataTransfer.setData("originStepId", originStepId);
@@ -46,7 +96,10 @@ function HomeController() {
     e.preventDefault();
     const leadId = e.dataTransfer.getData("leadId");
     const originStepId = e.dataTransfer.getData("originStepId");
-    if (parseInt(originStepId) + 1 !== stepId && parseInt(originStepId) !== stepId) {
+    if (
+      parseInt(originStepId) + 1 !== stepId &&
+      parseInt(originStepId) !== stepId
+    ) {
       toast.error("Leads só podem ser arrastados para a próxima etapa!", {
         position: "bottom-right",
         autoClose: 5000,
@@ -70,14 +123,49 @@ function HomeController() {
     updateLeadStep(leadId, stepId);
   };
 
-
-
   if (loading) {
     return <Loading />;
   }
 
-  return <Home steps={STEPS}leadsList={leadsList} handleDragOver={handleDragOver}
-    handleDrop={handleDrop} handleDragStart={handleDragStart} showLeadModal={showLeadModal} handleLeadModal={handleLeadModal}/>;
+  function closeLeadModal() {
+    setShowNewLeadModal(false);
+    setLeadForm({
+    name: "",
+    email: "",
+    phone: "",
+    opportunities: [],
+  })
+  }
+  function addNewLead(e) {
+    e.preventDefault();
+    try{
+        const newLeadsList = postNewLead(leadForm, user.id);
+        setLeadsList(newLeadsList);
+        console.log(leadsList)
+        closeLeadModal()
+    }catch(err){
+
+    }
+ 
+  }
+
+  return (
+    <Home
+      steps={STEPS}
+      leadsList={leadsList}
+      handleDragOver={handleDragOver}
+      handleDrop={handleDrop}
+      handleDragStart={handleDragStart}
+      showNewLeadModal={showNewLeadModal}
+      showLeadModal={showLeadModal}
+      setShowLeadModal={setShowLeadModal}
+      openNewLeadModal={openNewLeadModal}
+      leadForm={leadForm}
+      handleLeadForm={handleLeadForm}
+      closeLeadModal={closeLeadModal}
+      addNewLead={addNewLead}
+    />
+  );
 }
 
 export default HomeController;
